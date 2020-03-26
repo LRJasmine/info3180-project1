c2b3127cd01ddde4e19a42b0f129d46f1814f118
Ubuntu <ubuntu@ip-172-31-40-77.ec2.internal>
2020-03-26 21:53:02 +0000

    edit textarea

--- app/profileform.py
+++ app/profileform.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, SelectField, TextAreaField, validators
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length
 
 
 class AddProfileForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
-    gender =  SelectField('Gender', choices=[('male','Male'),('female','Female')], validators=[DataRequired()])
+    gender =  SelectField('Gender', default='Select Gender', choices=[('default','Select Gender'),('male','Male'),('female','Female')], validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     location = StringField('Location', validators=[DataRequired()])
-    biography = TextAreaField('Biography', validators=[DataRequired()])
+    biography = TextAreaField('Biography', validators=[DataRequired(), Length(max=250)])
     profilephoto = FileField('Profile Picture', validators=[
         FileRequired(),
         FileAllowed(['jpg', 'png', 'Images only!'])
